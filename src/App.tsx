27298323f7579dc420fbe6129ef499e8bc2ac3bd
WilliamArnone <arnonewilliam@gmail.com>
2024-06-17 18:47:40 +0200

    added accessible geometries

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import CounterLabel from "@components/dom/CounterLabel";
+import { A11yAnnouncer } from "@react-three/a11y";
 import Experience from "@components/Experience";
 import { Leva } from "leva";
 import { Perf } from "r3f-perf";
@@ -11,13 +11,12 @@ function App() {
     <>
       <Leva oneLineLabels collapsed hidden={!DEBUG} />
 
-      <CounterLabel />
-
       <div className="absolute left-0 top-0 h-svh w-full select-none p-0">
         <Canvas>
           <Experience />
           {DEBUG && <Perf position={"top-left"} />}
         </Canvas>
+        <A11yAnnouncer />
       </div>
     </>
   );
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,53 @@
-import { OrbitControls } from "@react-three/drei";
-import ClickableCube from "./three/ClickableCube";
+import { A11y, A11ySection } from "@react-three/a11y";
+import { Float } from "@react-three/drei";
 
 export default function Experience() {
   return (
     <>
-      <OrbitControls />
-      <ClickableCube />
+      <A11ySection label="Experience" description="3 accessible geometries">
+        <group position={[-2, 0, 0]}>
+          <A11y
+            role="content"
+            description="Orange Sphere"
+            focusCall={() => console.log("Orange Sphere")}
+          >
+            <Float>
+              <mesh>
+                <sphereGeometry args={[0.5, 32, 32]} />
+                <meshBasicMaterial color={"orange"} />
+              </mesh>
+            </Float>
+          </A11y>
+        </group>
+        <group>
+          <A11y
+            role="content"
+            description="Green Cube"
+            focusCall={() => console.log("Green Cube")}
+          >
+            <Float>
+              <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshBasicMaterial color={"green"} />
+              </mesh>
+            </Float>
+          </A11y>
+        </group>
+        <group position={[2, 0, 0]}>
+          <A11y
+            role="content"
+            description="Blue Cone"
+            focusCall={() => console.log("Blue Cone")}
+          >
+            <Float>
+              <mesh>
+                <coneGeometry args={[0.5, 1, 32]} />
+                <meshBasicMaterial color={"blue"} />
+              </mesh>
+            </Float>
+          </A11y>
+        </group>
+      </A11ySection>
     </>
   );
 }
